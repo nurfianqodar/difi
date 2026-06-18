e95aa009001669d927b92c15a55896fa7bc2b2b6
nurfianqodar <77nurfianqodar@gmail.com>
2026-06-18 19:03:09 +0700

    migrate to hash router

--- src/router.tsx
+++ src/router.tsx
@@ -1,26 +1,26 @@
-import { createBrowserRouter } from "react-router";
+import { createHashRouter } from "react-router";
 import { Dashboard } from "./pages/dashboard";
 import { Bd22Nd } from "./pages/birthday/bd22nd";
 
-export const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Dashboard />,
-  },
-  {
-    path: "/galery",
-    element: <div>Galery</div>,
-  },
-  {
-    path: "/mnj",
-    element: <div>Mnj</div>,
-  },
-  {
-    path: "/letters",
-    element: <div>Can&apos;t Sleep</div>,
-  },
-  {
-    path: "/bd/22",
-    element: <Bd22Nd />,
-  },
+export const router = createHashRouter([
+    {
+        path: "/",
+        element: <Dashboard />,
+    },
+    {
+        path: "/galery",
+        element: <div>Galery</div>,
+    },
+    {
+        path: "/mnj",
+        element: <div>Mnj</div>,
+    },
+    {
+        path: "/letters",
+        element: <div>Can&apos;t Sleep</div>,
+    },
+    {
+        path: "/bd/22",
+        element: <Bd22Nd />,
+    },
 ]);
